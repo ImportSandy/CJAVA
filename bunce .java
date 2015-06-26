e12e559106331a54980217669e402760d1831e2d
ImportSandy <sunnyrathee1994@gmail.com>
2015-06-27 02:56:27 +0530

    CODE FOR BOUNCING BALL>

---
+++ bunce .java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+public class bunce extends Applet {
+
+	
+	public void paint(Graphics g)
+	{
+		
+		g.fillOval(100 ,50, 50, 50);
+		//g.setColor(getBackground());
+		
+	}
+
+	
+
+}
